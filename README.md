e8e8aa51fab4f5989abba97ca62030c780f1b7d6
ben <nofe4rz@gmail.com>
2020-05-18 16:08:04 +0200

    added socket status & cleanup

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # node-muh
 homeautomation
+
+## Installation
+npm install --save
+npm rebuild
+
+## TODO
+* add notification on click
+* send mqtt message
--- express.js
+++ express.js
@@ -30,12 +30,12 @@ io.on('connection', async (socket) => {
   });
 
   var servero = { 'hosts' : [
-			{ id:1, name:'google.de', port:'80', mac:'20:DE:20:DE:20:DE'},
-			{ id:2, name:'google.it', port:'82', mac:'20:DE:20:DE:20:DE'},
-			{ id:3, name:'google.com', port:'80', mac:'20:DE:20:DE:20:DE'},
-			{ id:4, name:'google.com', port:'443', mac:'20:DE:20:DE:20:DE'},
-			{ id:5, name:'c3p1.muh', port:'22', mac:'40:8D:5C:1D:54:9B'},
-			{ id:6, name:'jabba.muh', port:'22', mac:'90:1B:0E:3E:F3:77'}
+			{ name:'google.com', port:'80', mac:'20:DE:20:DE:20:DE'},
+			{ name:'c3p1.muh', port:'22', mac:'40:8D:5C:1D:54:9B'},
+			{ name:'jabba.muh', port:'22', mac:'90:1B:0E:3E:F3:77'},
+			{ name:'p1.muh', port:'22', mac:'40:8D:5C:1D:54:9B'},
+			{ name:'p3.muh', port:'22', mac:'00:11:22:33:44:55'},
+			{ name:'p30.muh', port:'22', mac:'00:11:22:33:44:55'}
 		]}
   
   for (x in servero){
@@ -43,26 +43,25 @@ io.on('connection', async (socket) => {
 	  servero[x][y].state = await isReachable(servero[x][y].name + ':' + servero[x][y].port);
 	}
   }
-
-  console.log('Sending ...');
-  socket.emit('wol',servero); // send 
-  socket.on('wol', async (data) => { //get light switch status from client
+  console.log('[WOL] Sending JSON ...');
+  console.log('[WOL] JSON: ' + JSON.stringify(servero));
+  socket.emit('wol',servero); // send
+  
+  /*socket.on('wol', async (data) => { //get light switch status from client
 	for (x in servero){
 	  console.log('Starting ...');
 	  console.log('WOL: ' + servero[x]);
     }
-  }); 
+  });*/
 
   var interval = setInterval(async() => {
-	console.log('Interval ...');
+	console.log('[WOL] Sending JSON Interval ...');
   	for (x in servero){
 	  for (y in servero[x]){
 	    servero[x][y].state = await isReachable(servero[x][y].name + ':' + servero[x][y].port);
 	  }
 	}
-	for (x in servero){
-	  console.log('WOL: ' + JSON.stringify(servero[x]));
-	}
+	console.log('[WOL] JSON: ' + JSON.stringify(servero));
 	socket.emit('wol',servero); // send
   },5000);  
   
@@ -76,11 +75,6 @@ app.get('/ping', (req, res) => {
   res.send('pong');
 });
 
-app.get('/online', (req, res) => {
-  console.log(isReachable('google.com:443'));
-  res.send('OK');
-});
-
 app.get('/wol', (req, res) => {
   res.sendFile(__dirname + '/public/index.html')
 });
@@ -90,6 +84,6 @@ app.get('/wetter', (req, res) => {
 });
 
 server.listen(server_port, function () {
-  console.log('Example app listening on port: ' + server_port);
+  console.log('Listening on port: ' + server_port);
 });
 
