36874b01b95b4835dd3ef62d6f2bc9cbe707d0e2
13 <13@users.noreply.github.com>
2020-12-11 15:13:44 +0100

    add env.js sample

---
+++ env-sample.js
@@ -0,0 +1,22 @@
+// InfluxDB v2
+const url = process.env['INFLUX_URL'] || 'http://localhost:8086'
+const token = process.env['INFLUX_TOKEN'] || ''
+const org = process.env['INFLUX_ORG'] || 'muh'
+const bucket = 'homeautomation'
+// InfluxDB v1.8
+const username = ''
+const password = ''
+const token18 = `${username}:${password}`
+// Pushover
+const po_user = ''
+const po_token = ''
+
+module.exports = {
+  url,
+  token,
+  org,
+  bucket,
+  token18,
+  po_user,
+  po_token
+}
